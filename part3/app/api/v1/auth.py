1f730e7aa3f1c833fc195be1a80b90cf964ddbd4
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-21 19:07:57 +0300

    Update auth.py

--- part3/app/api/v1/auth.py
+++ part3/app/api/v1/auth.py
@@ -11,18 +11,22 @@ login_model = api.model('Login', {
 
 @api.route('/login')
 class Login(Resource):
-    @api.expect(login_model)
-    def post(self):
-        credentials = api.payload
-        
-        user = facade.get_user_by_email(credentials['email'])
-        
-        if not user or not user.verify_password(credentials['password']):
-            return {'error': 'Invalid credentials'}, 401
-        
-        access_token = create_access_token(
-            identity=str(user.id),
-            additional_claims={"is_admin": user.is_admin}
-        )
-        
-        return {'access_token': access_token}, 200
+	@api.expect(login_model)
+	def post(self):
+		"""Authenticate user and return a JWT token"""
+		credentials = api.payload  # Get the email and password from the request payload
+		
+		# Retrieve the user based on the provided email
+		user = facade.get_user_by_email(credentials['email'])
+		
+		# Check if the user exists and the password is correct
+		if not user or not user.verify_password(credentials['password']):
+			return {'error': 'Invalid credentials'}, 401
+		try:
+			# Create a JWT token with the user's id and is_admin flag
+			access_token = create_access_token(identity=user.id, additional_claims={'is_admin': user.is_admin})
+		except Exception as e:
+			return {'error': str(e).strip("'")}, 500
+
+		# Return the JWT token to the client
+		return {'access_token': access_token}, 200
