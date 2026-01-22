74eae40162bed649bd0e089cb4c3c053730191fc
Reemabdu213 <11972@holbertonstudents.com>
2026-01-22 17:50:45 +0300

    Update user.py

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,19 +1,29 @@
 import uuid
-from werkzeug.security import generate_password_hash, check_password_hash
+from app import bcrypt
 
 class User:
-    def __init__(self, first_name, last_name, email, password, is_admin=False):
+    def __init__(self, first_name, last_name, email, password=None, is_admin=False):
         self.id = str(uuid.uuid4())
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.password = generate_password_hash(password)
+        self.password = None
         self.is_admin = is_admin
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
-
+        
+        # Hash password if provided
+        if password:
+            self.hash_password(password)
+    
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+    
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
+    
     def to_dict(self):
+        """Convert user object to dictionary, excluding password."""
         return {
             "id": self.id,
             "first_name": self.first_name,
