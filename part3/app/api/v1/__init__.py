929e3c2ba1b01738b3c26671fcf0d7052e4aed0a
Reemabdu213 <11972@holbertonstudents.com>
2026-01-25 18:36:49 +0300

    Update __init__.py

--- part3/app/api/v1/__init__.py
+++ part3/app/api/v1/__init__.py
@@ -3,11 +3,11 @@ from flask_restx import Api
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.auth import api as auth_ns
-from app.api.v1.protected import api as protected_ns
+# from app.api.v1.protected import api as protected_ns
 
 api_v1_bp = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 api = Api(api_v1_bp, title='HBnB API', version='1.0')
 
 api.add_namespace(users_ns)
 api.add_namespace(auth_ns)
-api.add_namespace(protected_ns)
+# api.add_namespace(protected_ns)
