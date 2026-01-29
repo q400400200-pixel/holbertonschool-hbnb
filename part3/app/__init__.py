55d2969c272e274dc98f31cef271ed2c4869695d
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-29 14:58:50 +0300

    Update __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -5,10 +5,12 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from config import DevelopmentConfig
+from flask_sqlalchemy import SQLAlchemy
+
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
-
+db = SQLAlchemy()
 def create_app(config_class=DevelopmentConfig): 
     """Create and configure the Flask application"""
     
@@ -17,7 +19,7 @@ def create_app(config_class=DevelopmentConfig):
     
     bcrypt.init_app(app)
     jwt.init_app(app)
-    
+    db.init_app(app)
     # Register the API Blueprint
     from app.api.v1 import api_v1_bp
     app.register_blueprint(api_v1_bp)
