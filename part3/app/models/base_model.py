b802a464acfbd6b859952b7449da5f5a5700743b
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-29 15:06:51 +0300

    Update base_model.py

--- part3/app/models/base_model.py
+++ part3/app/models/base_model.py
@@ -2,12 +2,19 @@
 """
 BaseModel
 """
+from app import db
 import uuid
 from datetime import datetime
 
 
-class BaseModel:
+class BaseModel(db.Model):
     """ The main class for the models"""
+    __abstract__ = True  # This ensures SQLAlchemy does not create a table for BaseModel
+    
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
     
     def __init__(self):
         """Initialize the base model"""
