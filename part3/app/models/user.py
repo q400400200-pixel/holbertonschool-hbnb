59d87bc927b3b0a3a100adc0b7f1bae6ce52b015
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2026-01-29 17:02:18 +0300

    Update user.py

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -10,17 +10,21 @@ class User(BaseModel):
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-   # def __init__(self, first_name, last_name, email, password=None, is_admin=False):
-    #    self.id = str(uuid.uuid4())
-    #    self.first_name = first_name
-    #    self.last_name = last_name
-    #    self.email = email
-    #    self.password = None
-   #     self.is_admin = is_admin
+    # Relationships
+    places = db.relationship('Place', back_populates='owner', cascade='all, delete-orphan')
+    reviews = db.relationship('Review', back_populates='user', cascade='all, delete-orphan')
+    
+   """ def __init__(self, first_name, last_name, email, password=None, is_admin=False):
+        self.id = str(uuid.uuid4())
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = None
+        self.is_admin = is_admin
         
         # Hash password if provided
-     #   if password:
-    #        self.hash_password(password)
+        if password:
+            self.hash_password(password) """
     
     def hash_password(self, password):
         """Hashes the password before storing it."""
